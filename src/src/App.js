992826bbcc37476a1e4d84bbdd2821dc0db42386
Ebenézer Silva <enebeze@hotmail.com>
2018-02-01 19:49:21 -0200

    Estilizando o projeto.

--- src/src/App.js
+++ src/src/App.js
@@ -1,21 +1,218 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Button,
+  Item,
+  Comment,
+  Form
+} from "semantic-ui-react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const FixedMenuLayout = () => (
+  <div>
+    <Menu style={{ background: "#02acfe" }} fixed="top" inverted>
+      <Container>
+        <Menu.Item as="a" header>
+          <Image size="mini" src="/favicon.ico" style={{ marginRight: "1.5em" }} />
+          Leitura Udacity by Ebenézer
+        </Menu.Item>
+        <Menu.Item as="a">Home</Menu.Item>
 
-export default App;
+        <Dropdown item simple text="Dropdown">
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className="dropdown icon" />
+              <span className="text">Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Menu.Item position="right">
+          <Button as="a" primary style={{ marginLeft: "0.5em" }}>
+            Sign Up
+          </Button>
+        </Menu.Item>
+      </Container>
+    </Menu>
+
+    <Container>adfad</Container>
+
+    <Container text style={{ marginTop: "7em" }}>
+      <Header as="h1">Semantic UI React Fixed Template</Header>
+      <p>This is a basic fixed menu template using fixed size containers.</p>
+      <p>
+        A text container is used for the main container, which is useful for
+        single column layouts.
+      </p>
+      <Item.Group>
+        <Item>
+          {/* <Item.Image size="tiny" src="/images/image.png" /> */}
+
+          <Item.Content>
+            <Item.Header as="a">Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src="/images/short-paragraph.png" />
+            </Item.Description>
+            <Item.Extra>Additional Details</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          {/* <Item.Image size="tiny" src="/images/image.png" /> */}
+
+          <Item.Content>
+            <Item.Header as="a">Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src="/images/short-paragraph.png" />
+            </Item.Description>
+            <Item.Extra>Additional Details</Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+      <Comment.Group>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
+
+        <Comment>
+          <Comment.Avatar src="/images/avatar/small/matt.jpg" />
+          <Comment.Content>
+            <Comment.Author as="a">Matt</Comment.Author>
+            <Comment.Metadata>
+              <div>Today at 5:42PM</div>
+            </Comment.Metadata>
+            <Comment.Text>How artistic!</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+
+        <Comment>
+          <Comment.Avatar src="/images/avatar/small/elliot.jpg" />
+          <Comment.Content>
+            <Comment.Author as="a">Elliot Fu</Comment.Author>
+            <Comment.Metadata>
+              <div>Yesterday at 12:30AM</div>
+            </Comment.Metadata>
+            <Comment.Text>
+              <p>This has been very useful for my research. Thanks as well!</p>
+            </Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+          <Comment.Group>
+            <Comment>
+              <Comment.Avatar src="/images/avatar/small/jenny.jpg" />
+              <Comment.Content>
+                <Comment.Author as="a">Jenny Hess</Comment.Author>
+                <Comment.Metadata>
+                  <div>Just now</div>
+                </Comment.Metadata>
+                <Comment.Text>Elliot you are always so right :)</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+        </Comment>
+
+        <Comment>
+          <Comment.Avatar src="/images/avatar/small/joe.jpg" />
+          <Comment.Content>
+            <Comment.Author as="a">Joe Henderson</Comment.Author>
+            <Comment.Metadata>
+              <div>5 days ago</div>
+            </Comment.Metadata>
+            <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+
+        <Form reply>
+          <Form.TextArea />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+          />
+        </Form>
+      </Comment.Group>
+    </Container>
+
+    <Segment
+      inverted
+      vertical
+      style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+    >
+      <Container textAlign="center">
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header inverted as="h4" content="React JS" />
+              <List link inverted>
+              <List.Item as="a">ReactJS - Facebook</List.Item>
+                <List.Item as="a">Blog ReactJS</List.Item>
+                <List.Item as="a">News ReactJS</List.Item>
+                
+              </List>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header inverted as="h4" content="Bibliotecas" />
+              <List link inverted>
+              <List.Item as="a">Semantict UI React</List.Item>
+              <List.Item as="a">React Router Dom</List.Item>
+              <List.Item as="a">Redux</List.Item>
+              <List.Item as="a">Firebase</List.Item>
+              </List>
+            </Grid.Column>
+            
+          </Grid.Row>
+        </Grid>
+
+        <Divider inverted section />
+        <List horizontal inverted divided link>
+          <List.Item as="a" href="#">
+            Site Map
+          </List.Item>
+          <List.Item as="a" href="#">
+            Contact Us
+          </List.Item>
+          <List.Item as="a" href="#">
+            Terms and Conditions
+          </List.Item>
+          <List.Item as="a" href="#">
+            Privacy Policy
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
+  </div>
+);
+
+export default FixedMenuLayout;
+
+//export default App;
