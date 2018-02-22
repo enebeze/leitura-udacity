546148efa034ab4e69bbe50da3cbee51e1aafde4
Ebenezer <enebeze@hotmail.com>
2018-02-22 00:31:57 -0300

    Error for excluded page fixed.

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Home from "./views/Home";
-import Details from "./views/Details";
 import FormPost from "./components/FormPost";
 
 export default () => (
